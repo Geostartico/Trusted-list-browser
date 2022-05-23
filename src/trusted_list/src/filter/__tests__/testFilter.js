2612ef3b248a971b274c3ef6c726d7fff14845d3
giulpig <giulio.codutti20@gmail.com>
2022-05-23 16:33:25 +0200

    writing filter test, started generating docs

--- src/trusted_list/src/filter/__tests__/testFilter.js
+++ src/trusted_list/src/filter/__tests__/testFilter.js
@@ -8,9 +8,12 @@ describe('Filter test', function() {
     let myFilter;
     let all_providers = (objectify(Data.countryDict, Data.serviceDict)["servicesArray"]);
 
+<<<<<<< Updated upstream
     //console.log(all_providers);
     //console.log(Data.serviceDict);
 
+=======
+>>>>>>> Stashed changes
     let all_service_number = 0;
     for(let provider of Data.serviceDict){
         //console.log(provider["services"]);
@@ -18,6 +21,7 @@ describe('Filter test', function() {
             all_service_number += 1;
     }
 
+<<<<<<< Updated upstream
     describe('Filter construction', function() {
         it('should construct the Filter object', function(){
             myFilter = new Filter([]);
@@ -31,12 +35,28 @@ describe('Filter test', function() {
         it('should return all services when I filter (comparing the number of entries)', function(){
             //console.log(all_service_number);
             assert.equal(myFilter.getFiltered().services.getSize(), all_service_number);
+=======
+    describe('Filter testing', function() {
+        describe('Filter construction', function() {
+            it('should construct the Filter object', function(){
+                myFilter = new Filter([]);
+                myFilter = new Filter(all_providers);
+            });
+            it('sould have nothing selected', function(){
+                for(let map of [myFilter.selected.providers, myFilter.selected.statuses, myFilter.selected.countries, myFilter.selected.types]){
+                    assert.equal(map.getSize(), 0);
+                }
+            });
+            it('should return all services when I filter (comparing the number of entries)', function(){
+                assert.equal(myFilter.getFiltered().services.getSize(), all_service_number);
+            });
+>>>>>>> Stashed changes
         });
-        it('should fuck your uncle in the ass', function(){
-            myFilter.addRule(new Rule(all_providers[0].getCountry()));
-        });
-    });
+        describe('Adding rules', function() {
+            it('should return the filtered services by status (comparing number of entries)', function(){
+                myFilter = new Filter(all_providers);
 
+<<<<<<< Updated upstream
     for(let service of Data.serviceDict){
         let all_good = true;
         for(let service2 of all_providers){
@@ -49,5 +69,46 @@ describe('Filter test', function() {
             //console.log(service.name);
         }
     }
+=======
+                let my_status = all_providers[3].status;
+                myFilter.addRule(new Rule(my_status));
+                let sum = 0;
+                for(let provider of all_providers){
+                    if(provider.status == my_status){
+                        sum += 1;
+                    }
+                }
+                assert.equal(myFilter.getFiltered().services.getSize(), sum);
+            });
+            it('should return the filtered services by country (comparing number of entries)', function(){
+                myFilter = new Filter(all_providers);
 
+                let my_country = all_providers[3].getCountry();
+                myFilter.addRule(new Rule(my_country));
+                let sum = 0;
+                all_providers[0].getCountry().getProviders().forEach((provider) => {
+                    sum += provider.getServices().getSize();
+                    //console.log(provider.getServices());
+                });
+                assert.equal(myFilter.getFiltered().services.getSize(), sum);
+            });
+            it('should return the filtered services by type (comparing number of entries)', function(){
+                myFilter = new Filter(all_providers);
+>>>>>>> Stashed changes
+
+                let my_type = all_providers[3].getServiceTypes()[0];
+                myFilter.addRule(new Rule(my_type));
+                let sum = 0;
+                for(let provider of all_providers){
+                    for(let service of provider.getChildren().values()){
+                        service.getChildren().forEach(() => {});
+                        if(service.getChildren().includes(my_type)){
+                            sum += 1;
+                        }
+                    }
+                }
+                assert.equal(myFilter.getFiltered().services.getSize(), sum);
+            });
+        });
+    });
 });
