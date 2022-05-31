d4095075dc26ae69cb630dc25b4b71d37f9e88a0
LK-parrot <69172776+LK-parrot@users.noreply.github.com>
2022-05-31 18:27:36 +0200

    Update fetch.tsx

--- src/trusted_list/src/fetch/fetch.tsx
+++ src/trusted_list/src/fetch/fetch.tsx
@@ -1,32 +1,19 @@
 export class Fetcher{
 	
-	private counter;
-
-	public constructor(){
 	
-		this.counter = 0;
-
-	}
 
 	async getJSON (url: string) {
 
 	return await fetch(url)
 	.then(res => {
 
-			if(!res.ok){
-                		
-				console.log("tua mamma");
-				
-				if (this.counter < 2){
-					this.counter += 1;
-					//return await this.getJSON(url);
-				}
-				else{
-				    this.counter = 0;
-					throw new Error('Can\'t fetch json');
-                }
+			if(!res.ok)
+                throw new Error('Can\'t fetch json');
+                
         	}
         	res.json();
+        }).catch(error => {
+            return await this.fetch(url)
         });
 	}
 }
